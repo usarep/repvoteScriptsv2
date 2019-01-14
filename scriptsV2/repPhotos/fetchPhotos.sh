d20d43ee47cf415c81f9526e716151275ea433f5
am <am@MacBook-Pro.local>
2019-01-13 17:12:58 -0800

    some changes for updating new data

--- scriptsV2/repPhotos/fetchPhotos.sh
+++ scriptsV2/repPhotos/fetchPhotos.sh
@@ -6,7 +6,7 @@
 # perl downloadRepPhotos.pl XYZ215 out
 
 DB_PASSWD=$1
-OUT_DIR=out
+OUT_DIR=out2
 
 perl fetchPhotos.pl $DB_PASSWD $OUT_DIR
 
--- scriptsV2/senate/rollCallYearLoop.sh
+++ scriptsV2/senate/rollCallYearLoop.sh
@@ -2,8 +2,8 @@
 # CONGRESS_START=108; # 2003
 # CONGRESS_END=114;  # 2016
 
-CONGRESS_START=115; # 2017
-CONGRESS_END=115;  # 2017
+CONGRESS_START=116; # 2019
+CONGRESS_END=116;  # 2019
 
 dbPasswd=$1;
 
@@ -11,7 +11,7 @@ for congress in $(seq -f "%03g" $CONGRESS_START $CONGRESS_END); do
     echo congress=${congress} ;
 
     # for sessionInt in 1 2; do
-    for sessionInt in 2 ; do
+    for sessionInt in 1 ; do
 
     echo session=${sessionInt}
     bash -f ./rollCall.sh ${congress}  ${sessionInt} ${dbPasswd} ;
